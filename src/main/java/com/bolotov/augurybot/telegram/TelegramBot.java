cc42abff13666071ba82c3af085d61d99daf4466
bolotov-ip <innokenty.bolotov@gmail.com>
2023-01-31 22:09:26 +0800

    telegram 5% create module

---
+++ src/main/java/com/bolotov/augurybot/telegram/TelegramBot.java
@@ -0,0 +1,33 @@
+package com.bolotov.augurybot.telegram;
+
+import com.bolotov.augurybot.config.BotConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final Logger log = LoggerFactory.getLogger(TelegramBot.class);
+
+    @Autowired
+    BotConfig config;
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
